993fd941bec3ecc00df2581c5ef48c9fd9526bf0
MarvinZ <marvinzzz@gmail.com>
2024-01-17 10:59:07 -0600

    generating redirect rules for other file

--- Redirector.py
+++ Redirector.py
@@ -1,24 +1,33 @@
 import pandas as pd
 
-def generate_redirect_rules(input_file, output_file):
+def generate_rewrite_map(input_file, output_file):
     # Read the Excel file
     df = pd.read_excel(input_file, usecols=['SLUG', 'NEW'])
 
     with open(output_file, 'w') as f:
+        # Write the opening tags
+        f.write('<rewriteMaps>\n')
+        f.write('  <rewriteMap name="Redirects">\n')
+
         for index, row in df.iterrows():
-            # Remove trailing slash from SLUG and NEW if present
-            slug = row['SLUG'].rstrip('/')
-            new_url = row['NEW'].rstrip('/')
+            # Ensure SLUG is a string and construct the key
+            slug = '/updates/' + str(row['SLUG']).strip('/')
+            slug2 = '/updates/' + str(row['SLUG']).strip('/') +'/'
             
-            # Construct the redirect rule
-            rule = f"""
-        <rule name="Redirect Rule {index + 1001}" stopProcessing="true">
-          <match url="^updates/{slug}/?$" />
-          <action type="Redirect" redirectType="Permanent" url="{new_url}" />
-        </rule>
-"""
-            # Write the rule to the file
-            f.write(rule)
+            # Ensure correct URL format in NEW, ensuring no double slashes
+            new_url = row['NEW'].replace('//', '/').replace('http:/', 'http://').replace('https:/', 'https://')
+            
+            # Construct the add entry
+            add_entry = f'    <add key="{slug}" value="{new_url}" />\n'
+            add_entry2 = f'    <add key="{slug2}" value="{new_url}" />\n'
+            
+            # Write the add entry to the file
+            f.write(add_entry)
+            f.write(add_entry2)
+
+        # Write the closing tags
+        f.write('  </rewriteMap>\n')
+        f.write('</rewriteMaps>\n')
 
 # Usage
-generate_redirect_rules('webconfigrules.xlsx', 'redirect_rules.txt')
+generate_rewrite_map('webconfigrules.xlsx', 'rewritemaps.config')
